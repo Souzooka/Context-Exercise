6334a50de1e6d92fecf15848310d37c38d2afed4
Dakota <bazookaharwell@gmail.com>
2017-03-18 18:12:36 -1000

    I can't read numbers

--- context.js
+++ context.js
@@ -17,10 +17,10 @@ var wall_e = new Robot("Wall-E");
 
 
 // #1
-console.log(add(1, 2));
+console.log(add(0, 1));
 
 // #2
-console.log(add.call(voltron, 1, 2));
+console.log(add.call(voltron, 2, 3));
 
 // #3
 console.log(add.apply(optimus, [20, 30]));
